9a446f47e426ec934c5e076d05edd305ec0f2cc5
oeddahab@emeal.nttdata.com <oeddahab@emeal.nttdata.com>
2024-10-21 13:23:58 +0200

    Este el primer commit para añadir los archivos del proyecto al repositorio local

---
+++ src/main/java/com/nttdata/nttdatacenters_jdbc_t1_OEDL/App.java
@@ -0,0 +1,13 @@
+package com.nttdata.nttdatacenters_jdbc_t1_OEDL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
